7ef7f0b92a5b6bab4adcd9b1f5fae706ccf36902
Trọng Bình <trongbbfx02929@funix.edu.vn>
2021-01-30 15:35:10 +0700

    new code

---
+++ PRM391x_02_VN_Project2_trongbbfx02929funixeduvn/app/src/main/java/com/example/prm391x_02_vn_project2_trongbbfx02929funixeduvn/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.prm391x_02_vn_project2_trongbbfx02929funixeduvn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
