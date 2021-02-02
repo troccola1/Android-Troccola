413333dbad0bb0e8199a6ac11d18f40cafddf4fb
Trọng Bình <trongbbfx02929@funix.edu.vn>
2021-02-02 09:43:55 +0700

    new code

--- PRM391x_02_VN_Project2_trongbbfx02929funixeduvn/app/src/main/java/com/example/prm391x_02_vn_project2_trongbbfx02929funixeduvn/MainActivity.java
+++ PRM391x_02_VN_Project2_trongbbfx02929funixeduvn/app/src/main/java/com/example/prm391x_02_vn_project2_trongbbfx02929funixeduvn/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.prm391x_02_vn_project2_trongbbfx02929funixeduvn;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,88 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Khai báo thanh tiêu đề
+        final ActionBar actionBar = getSupportActionBar();
+        assert actionBar != null;
+        actionBar.setTitle("Tour Guide App");
+
+        // Khai báo nút chọn ảnh
+        final ImageButton hotel = findViewById(R.id.image_button_hotel);
+        final ImageButton hospital = findViewById(R.id.image_button_hospital);
+        final ImageButton atm = findViewById(R.id.image_button_atm);
+        final ImageButton metro = findViewById(R.id.image_button_metro);
+
+        // Khai báo đoạn đối tượng
+        final Fragment fragmentHotel = new HotelFragment();
+        final Fragment fragmentHospital = new HospitalFragment();
+        final Fragment fragmentAtm = new AtmFragment();
+        final Fragment fragmentMetro = new MetroFragment();
+
+        // Nút hình ảnh hotel
+        hotel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.layout, fragmentHotel);
+                fragmentTransaction.commit();
+                actionBar.setDisplayHomeAsUpEnabled(true);
+                actionBar.setTitle("Hotel");
+                hotel.setVisibility(View.GONE);
+                hospital.setVisibility(View.GONE);
+                atm.setVisibility(View.GONE);
+                metro.setVisibility(View.GONE);
+            }
+        });
+
+        hotel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.layout, fragmentHospital);
+                fragmentTransaction.commit();
+                actionBar.setDisplayHomeAsUpEnabled(true);
+                actionBar.setTitle("Hotel");
+                hotel.setVisibility(View.GONE);
+                hospital.setVisibility(View.GONE);
+                atm.setVisibility(View.GONE);
+                metro.setVisibility(View.GONE);
+            }
+        });
+
+        hotel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.layout, fragmentAtm);
+                fragmentTransaction.commit();
+                actionBar.setDisplayHomeAsUpEnabled(true);
+                actionBar.setTitle("Hotel");
+                hotel.setVisibility(View.GONE);
+                hospital.setVisibility(View.GONE);
+                atm.setVisibility(View.GONE);
+                metro.setVisibility(View.GONE);
+            }
+        });
+
+        hotel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.layout, fragmentMetro);
+                fragmentTransaction.commit();
+                actionBar.setDisplayHomeAsUpEnabled(true);
+                actionBar.setTitle("Hotel");
+                hotel.setVisibility(View.GONE);
+                hospital.setVisibility(View.GONE);
+                atm.setVisibility(View.GONE);
+                metro.setVisibility(View.GONE);
+            }
+        });
     }
+
 }
